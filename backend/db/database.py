ccb2a0768ccdcc6e06989d9204f0020617df87b5
Mikael <kaelbaracho@gmail.com>
2024-09-03 17:59:56 -0300

    Removing backend for tests

--- backend/db/database.py
+++
@@ -1,6 +0,0 @@
-from peewee import SqliteDatabase
-
-# SqliteDatabase -> É uma classe que representa uma conexão com um banco de dados sqlite
-
-# sqlite -> É muito simples com biblioteca. Ele é projetado para ser fácil de usar e integrar em aplicaçõees
-db = SqliteDatabase('server.db')
