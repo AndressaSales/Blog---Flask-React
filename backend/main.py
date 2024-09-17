01e74acf2a1bde0bcf654aa1762caa8b14d3da41
AndressaSales <andressaccs2001@gmail.com>
2024-09-17 14:13:21 -0300

    atualizando o vercel

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 # importaçõa da pag models/user
-from api.user import user_routes
+from user import user_routes
 
 app = Flask(__name__)
 CORS(app)
