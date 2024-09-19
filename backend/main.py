3407d000c5acdca184693b95975a6d67c13a132b
AndressaSales <andressaccs2001@gmail.com>
2024-09-19 17:26:01 -0300

    ajeitando o main

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from models.user import user_routes
-from routes.home import home
+
 
 app= Flask(__name__)
 
-app.register_blueprint(home)
+@app.route('/')
+def home():
+    return "olá, Mundo!"
+
 app.register_blueprint(user_routes, url_prefix="/user")
 
 if __name__ == '__main__':
--- backend/routes/home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-home = Blueprint('Home', __name__)
-
-
-@home.route('/')
-def home_pag():
-    return "Olá, Mundo!"
